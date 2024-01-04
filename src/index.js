19dca6a10bd0d8cee23819dc54c0177bdbba05b5
Gabriel Soares ou O Editor <gabrielsoares1602@gmail.com>
2024-01-04 02:13:58 -0300

    added slash commands

--- src/index.js
+++ src/index.js
@@ -1,22 +1,50 @@
 import { config } from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Routes } from "discord.js";
+import { REST } from '@discordjs/rest';
 
 config();
 
-const client = new Client({ intents: [
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+const client = new Client({ 
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent
-] });
-const TOKEN = process.env.BOT_TOKEN;
+]});
 
-client.login(TOKEN);
+const rest = new REST({ version: '10'}).setToken(TOKEN);
 
-client.on("ready", () => {
-  console.log(`${client.user.displayName} has logged in`);
+client.on('ready', () => console.log(`${client.user.displayName} has logged in`));
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  if(interaction.commandName === "ping") {
+    await interaction.reply({content:'Pong!'});
+  }
 });
 
-client.on("messageCreate", (message) => {
-  console.log(message.content);
-  console.log(message.author.username);
-});
+async function main(){
+  const commands = [
+    {
+      name: 'ping',
+      description: 'Replies with Pong!',
+    }
+  ];
+  try {
+
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationCommands(CLIENT_ID, GUILD_ID), { 
+      body: commands
+    });
+    client.login(TOKEN);
+    console.log('Successfully reloaded application (/) commands.');
+
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+main();
