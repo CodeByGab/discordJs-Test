72bd32792b96de450179972b4a52f2c72d75a55c
Gabriel Soares ou O Editor <gabrielsoares1602@gmail.com>
2024-01-05 19:29:13 -0300

    separate command folder

---
+++ src/slashCommands/orderCommand.js
@@ -0,0 +1,45 @@
+import { SlashCommandBuilder } from "@discord.js/builders";
+
+const orderCommand = new SlashCommandBuilder()
+    .setName('order')
+    .setDescription('Order your favorite meal!')
+    .addStringOption((option) => 
+        option
+        .setName('food')
+        .setDescription('Select your favorite food')
+        .setRequired(true)
+        .setChoices(
+            {
+            name: 'Cake',
+            value: 'Cake',
+            }, {
+            name: 'Chocolate',
+            value: 'Chocolate',
+            }, {
+            name: 'Hamburguer',
+            value: 'Hamburguer',
+            },
+        )
+    )
+    .addStringOption((option) =>
+        option
+        .setName('drink')
+        .setDescription('Select your favorite drink')
+        .setRequired(false)
+        .setChoices(
+            {
+            name: 'Guaraná antártica',
+            value: 'guaraná',
+            },
+            {
+            name: 'Coca cola',
+            value: 'coca-cola',
+            },
+            {
+            name: 'Sprite',
+            value: 'sprite',
+            },
+        )
+    )
+
+export default orderCommand.toJSON();
